1b237b8abec18dbc0c78f8e0d1b6d31a3b418ce8
Shubhankar Abhay Kamthankar <shubhankar.k@research.iiit.ac.in>
2024-06-06 12:39:12 +0530

    cs file upload testing

---
+++ C# Basics/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("I wonder what the difference between Console.WriteLine() and Console.Write() is?");
+Console.WriteLine("This is the first command that I'm writing using WriteLine Command. As you can see, this is in one line");
+Console.WriteLine("This is the 2nd command that I'm writing to using Write Line method. As you can see, this is in a different line");
